9578babd82eef24694aa7976d6c264d443b0ee84
Octavio Pozo Garcia <o_pozo@hotmail.com>
2016-07-18 23:04:11 +0300

    Add files via upload
    Work in progress, want to buiild  high dimensionality visualization,  but I have not found the right data set

---
+++ Webread/visexample.m
@@ -0,0 +1,55 @@
+%Weberead data visualization
+%
+%% Planet OS API
+apiPlanet = 'http://api.planetos.com/v1/';
+apikey = '4a80ef4d4b56437f835f1e820aaaa150';
+
+
+
+%% baltic surface temperature data
+dataset_id = 'myocean_sst_baltic_daily';
+count=50;
+urlPlanet2 = [apiPlanet 'datasets/' dataset_id '/point'];
+rdata = webread(urlPlanet2, 'apikey', apikey,'lat', 58.8,'lon', 18.96 , 'count', count);
+
+datapre=struct2cell(rdata.entries)';
+
+
+for i=1:size(datapre,1)
+    
+ rr{i} = (struct2cell(datapre{i,3}))';
+ rt(i) =  datetime(datapre{i,2}.time, 'InputFormat', 'uuuu-MM-dd''T''HH:mm:ss','TimeZone','UTC');
+
+end
+ % temperature in kelvin is in one
+for i=1:size(datapre,1)
+t     = cell2mat(rr{1,i});
+ts(i) = t(1);
+
+end
+figure
+plot(rt,ts)
+title(['Surface temperature of baltic sea'])
+
+
+%baltic beochemistry data
+dataset_id2 = 'copernicus_biogeo_baltic_hourly';
+count=50;
+urlPlanet3 = [apiPlanet 'datasets/' dataset_id2 '/point'];
+cdata = webread(urlPlanet3, 'apikey', apikey,'lat', 58.8,'lon', 18.96 , 'count', count);
+dpre=struct2cell(cdata.entries)';
+
+for i=1:size(dpre,1)
+    
+ cr{i} = (struct2cell(dpre{i,3}))';
+ ct(i) =  datetime(dpre{i,2}.time, 'InputFormat', 'uuuu-MM-dd''T''HH:mm:ss','TimeZone','UTC');
+c     = cell2mat(cr{1,i});
+ns(i) = c(1);
+np(i) = c(2);
+nc(i) = c(3);
+nd(i) = c(4);
+end
+for i=1:size(dpre,1)
+
+end
+chemicals=[ns; np; nc; nd];
