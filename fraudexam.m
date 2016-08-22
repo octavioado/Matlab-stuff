493f951a1605bce33f1b9681b24cf8f08c241694
Octavio Pozo Garcia <o_pozo@hotmail.com>
2016-08-22 10:57:30 +0300

    Update fraudexam.m

--- fraudexam.m
+++ fraudexam.m
@@ -12,7 +12,7 @@ trainO=numeric(1:500,21);
 test=numeric(501:end,1:20);
 testO=numeric(501:end,21);
 
-% classify with SVM
+% classify with SVM without feature selection
 
 svmodel= fitcsvm(train,trainO);
 [label,score] = predict(svmodel,test);
@@ -26,3 +26,20 @@ cvmd2 = crossval(ScoreSVMModel);
 misclass2 = kfoldLoss(cvmd2);
 
 
+% Prediction with Feature selection
+
+dataTrainG1 = train(grp2idx(trainO)==1,:);
+dataTrainG2 = train(grp2idx(trainO)==2,:);
+
+[h,p,ci,stat] = ttest2(dataTrainG1,dataTrainG2,'Vartype','unequal');
+ecdf(p);
+xlabel('P value');
+ylabel('CDF value')
+
+ftrain=[train(:,2),train(:,11),train(:,15:19)];
+ftest =[test(:,2),test(:,11),test(:,15:19)];
+svmodel= fitcsvm(ftrain,trainO);
+[label,score] = predict(svmodel,ftest);
+ScoreSVMModel = fitPosterior(svmodel,ftest,testO);
+cvmd3 = crossval(svmodel);
+misclass3 = kfoldLoss(cvmd3);
